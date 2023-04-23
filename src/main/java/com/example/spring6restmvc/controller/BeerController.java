22e2b1a0498a1f7f383ca7afc32e840e76d4e538
earl-stephens <stephensearl@hotmail.com>
2023-04-23 15:15:42 -0600

    Set up controller and service for post operation

--- src/main/java/com/example/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/example/spring6restmvc/controller/BeerController.java
@@ -3,7 +3,10 @@ package com.example.spring6restmvc.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +35,12 @@ public class BeerController {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Beer> listBeers() {
 		return beerService.listBeers();
+	}
+	
+	@PostMapping
+	public ResponseEntity handlePost(Beer beer) {
+		Beer savedBeer = beerService.saveNewBeer(beer);
+		return new ResponseEntity(HttpStatus.CREATED);
 	}
 }
 
