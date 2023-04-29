8c029dc5dd06ad05642b6e5bc9b2274c0c51aeb5
earl-stephens <stephensearl@hotmail.com>
2023-04-29 17:16:17 -0600

    Write test for POST for new customer

--- src/test/java/com/example/spring6restmvc/CustomerControllerTest.java
+++ src/test/java/com/example/spring6restmvc/CustomerControllerTest.java
@@ -1,13 +1,14 @@
 package com.example.spring6restmvc;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,16 +20,41 @@ import com.example.spring6restmvc.controller.CustomerController;
 import com.example.spring6restmvc.model.Customer;
 import com.example.spring6restmvc.services.CustomerService;
 import com.example.spring6restmvc.services.CustomerServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(CustomerController.class)
 public class CustomerControllerTest {
 	@Autowired
 	MockMvc mockMvc;
 	
+	@Autowired
+	ObjectMapper objectMapper;
+	
 	@MockBean
 	CustomerService customerService;
 	
-	CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+	CustomerServiceImpl customerServiceImpl;
+	
+	@BeforeEach
+	void setUp() {
+		customerServiceImpl = new CustomerServiceImpl();		
+	}
+	
+	@Test
+	void testCreateNewCustomer() throws JsonProcessingException, Exception {
+		Customer customer = customerServiceImpl.listCustomers().get(0);
+		customer.setId(null);
+		customer.setVersion(0);
+		
+		given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImpl.listCustomers().get(0));
+		
+		mockMvc.perform(post("/api/v1/customer").accept(MediaType.APPLICATION_JSON)
+												.contentType(MediaType.APPLICATION_JSON)
+												.content(objectMapper.writeValueAsString(customer)))
+												.andExpect(status().isCreated())
+												.andExpect(header().exists("Location"));
+		}
 	
 	@Test
 	void testGetCustomerById() throws Exception {
